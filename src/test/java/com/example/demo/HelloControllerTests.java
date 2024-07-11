ade48fcdbdbf331c91c003ca7552b1c531f46388
ElgarWicaksono <elgarwicak@gmail.com>
2024-07-11 23:00:41 +0700

    re init version 17

--- src/test/java/com/example/demo/HelloControllerTests.java
+++ src/test/java/com/example/demo/HelloControllerTests.java
@@ -13,7 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest(classes=DemoApplication.class)
+@SpringBootTest
 @AutoConfigureMockMvc
 public class HelloControllerTest {
 
